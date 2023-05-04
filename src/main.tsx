2c4a3c560ea7b53c51c77bb483f7e72156a822da
shota-tsuji <shotatuzi@gmail.com>
2023-05-04 13:59:06 +0900

    feat: add GraphQL query to cpp-backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createClient, Provider, cacheExchange, fetchExchange } from "urql";
 import App from './App.tsx'
 import './index.css'
 
+const client = createClient({
+  url: 'http://0.0.0.0:8080/',
+  exchanges: [cacheExchange, fetchExchange]
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+        <App />
+    </Provider>
   </React.StrictMode>,
 )
