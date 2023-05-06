8c194300c3d3c0cb2f6f950378f570ee7eb39d18
shota-tsuji <shotatuzi@gmail.com>
2023-05-06 22:54:18 +0900

    feat: set MainRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import './App.css'
 
-//import { useRecipesQuery, useRecipeDetailQuery } from './generated/graphql';
 import { useRecipesQuery } from './generated/graphql';
-import { MantineProvider, Table } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
+import { BrowserRouter as Router } from "react-router-dom";
+import MainRouter from './pages/MainRoutes';
 
 function App() {
   //const [result, _reexecuteQuery] = useRecipeDetailQuery({ variables: { recipeId: "00000000-0000-0000-0000-000000000000" } })
-  const [result, _reexecuteQuery] = useRecipesQuery();
-  const { data, fetching, error } = result;
-  console.log(JSON.stringify({ fetching, data, error }, null, 2));
-
-  if (error != null) {
-    return <>{JSON.stringify(error)}</>;
-  }
-  if (fetching) return <p>Loading...</p>;
 
   //let recipeDetail = data!.recipeDetail;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -23,23 +16,12 @@ function App() {
           //<ul>
 //{steps.map(step => (<li key={step.id}>{step.description}, {step.duration}</li>))}
           //</ul>
-  const recipes = data!.recipes.map((recipe) => (
-              <tr key={recipe.id}>
-                <td>{recipe.title}</td>
-              </tr>
-              ));
-
   return (
 <>
 <MantineProvider withGlobalStyles withNormalizeCSS>
-<Table>
-	<thead>
-		<tr>
-				<th>Recipe name</th>
-		</tr>
-	</thead>
-	<tbody>{recipes}</tbody>
-</Table>
+<Router>
+<MainRouter />
+</Router>
 </MantineProvider>
 
 </>
