e34a5dd43bcdd818ed4fb19f01b857b5c65200f2
shota-tsuji <shotatuzi@gmail.com>
2023-05-20 17:11:02 +0900

    Detail page (#6)
    * feat: format and use Link to recipe-detail
    
    * refactor: format
    
    * feat: show recipe details step with Mantine Timeline
    
    * refactor: remove unused css style
    
    * refactor: remove unused function

--- src/pages/recipes/recipeDetailPage.tsx
+++ src/pages/recipes/recipeDetailPage.tsx
@@ -1,33 +1,39 @@
 import React from "react";
-import { useRecipeDetailQuery } from "../../generated/graphql";
-import { Route, Routes, useParams } from "react-router-dom";
+import {useRecipeDetailQuery} from "../../generated/graphql";
+import {Route, Routes, useParams} from "react-router-dom";
 import RecipeDetailEdit from "../../features/recipes/RecipeDetailEdit";
+import {Center, Timeline, Title} from "@mantine/core";
 
 export default function RecipeDetailPage() {
-  //const { id } = useParams<{ te: string }>();
-  const params = useParams();
-  const [result, _reexecuteQuery] = useRecipeDetailQuery({ variables: { recipeId: params.recipeId! } });
-  const { data, fetching, error } = result;
+    //const { id } = useParams<{ te: string }>();
+    const params = useParams();
+    const [result, _reexecuteQuery] = useRecipeDetailQuery({variables: {recipeId: params.recipeId!}});
+    const {data, fetching, error} = result;
 
-  if (error != null) {
-    return <>{JSON.stringify(error)}</>;
-  }
-  if (fetching) return <p>Loading...</p>;
+    if (error != null) {
+        return <>{JSON.stringify(error)}</>;
+    }
+    if (fetching) return <p>Loading...</p>;
 
-  let recipeDetail = data!.recipeDetail;
-  let {title, description, steps, ...rest } = recipeDetail;
+    const recipeDetail = data!.recipeDetail;
+    const {title, description, steps, ...rest} = recipeDetail;
 
-  return (
-            <React.Fragment>
-          <p>{recipeDetail.id}: {recipeDetail.title}: {recipeDetail.description}</p>
-          <ul>
-{steps.map(step => (<li key={step.id}>{step.description}, {step.duration}</li>))}
-          </ul>
+    return (
+        <React.Fragment>
+            <Title order={1} mt="auto">{recipeDetail.title}</Title>
+            <Title order={2}>{recipeDetail.description}</Title>
+            <Center>
+                <Timeline active={1} bulletSize={32} lineWidth={4}>
+                    {steps.map(step => (<Timeline.Item title={step.description} key={step.id}>
+                        {step.duration}min: resource:{step.resourceId}
+                    </Timeline.Item>))}
+                </Timeline>
+            </Center>
             <div>
-            <Routes>
-            <Route path="edit" element={<RecipeDetailEdit />} />
-            </Routes>
+                <Routes>
+                    <Route path="edit" element={<RecipeDetailEdit/>}/>
+                </Routes>
             </div>
-            </React.Fragment>
-          );
+        </React.Fragment>
+    );
 }
