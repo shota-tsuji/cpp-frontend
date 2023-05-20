93f7dff0e8bf78c2bda49e0b51fac5e1d17927ed
shota-tsuji <shotatuzi@gmail.com>
2023-05-20 20:34:34 +0900

    feat: add resource edit button

--- src/pages/resources/ResourceIndexPage.tsx
+++ src/pages/resources/ResourceIndexPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {useResourcesQuery} from "../../generated/graphql";
 import {Link} from "react-router-dom";
-import {Paper, Table, Title} from "@mantine/core";
+import { useDisclosure } from '@mantine/hooks';
+import {Button, Group, Modal, NumberInput, Paper, Table, Title} from "@mantine/core";
 
 export default function ResourceIndexPage() {
+    const [opened, { open, close }] = useDisclosure(false);
     const [result, _reexecuteQuery] = useResourcesQuery();
     const {data, fetching, error} = result;
     console.log(JSON.stringify({fetching, data, error}, null, 2));
@@ -15,8 +17,22 @@ export default function ResourceIndexPage() {
 
     const resourceList = data!.resources.map(resource => (
         <tr key={resource.id}>
-            <td><Link to={resource.id}>{resource.name}</Link></td>
+            <td>{resource.name}</td>
             <td>{resource.amount}</td>
+            <td>
+                <Modal opened={opened} onClose={close} title="Authentication">
+                    <NumberInput
+                        label="Resource count"
+                        description="From 1 to 10"
+                        placeholder="Resource count"
+                        max={10}
+                        min={1}
+                    />
+                </Modal>
+                <Group position="center">
+                    <Button onClick={open}>Open modal</Button>
+                </Group>
+            </td>
         </tr>
     ));
 
@@ -29,6 +45,7 @@ export default function ResourceIndexPage() {
                     <tr>
                         <th>Name</th>
                         <th>Amount</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>{resourceList}</tbody>
