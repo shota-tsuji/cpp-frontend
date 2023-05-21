1ee324aba7050608630d09814a80ccd7cbc1550e
shota-tsuji <shotatuzi@gmail.com>
2023-05-21 22:09:30 +0900

    fix: modify recipes routing

--- src/pages/recipes/RecipeRoutes.tsx
+++ src/pages/recipes/RecipeRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Route, Routes} from "react-router-dom";
 import RecipeIndexPage from "./RecipeIndexPage";
 import RecipeDetailPage from "./recipeDetailPage";
+import RecipeDetailEdit from "../../features/recipes/RecipeDetailEdit";
 
 // use index route to distinguish index from matching other patterns.
 // https://reactrouter.com/en/main/route/route#index
@@ -10,8 +11,9 @@ export default function RecipeRouter() {
         <React.Fragment>
             <div>
                 <Routes>
-                    <Route index element={<RecipeIndexPage/>}/>
-                    <Route path=":recipeId/*" element={<RecipeDetailPage/>}/>
+                    <Route path="/" element={<RecipeIndexPage/>}/>
+                    <Route path=":recipeId/" element={<RecipeDetailPage/>}/>
+                    <Route path=":recipeId/edit" element={<RecipeDetailEdit/>}/>
                 </Routes>
             </div>
         </React.Fragment>
