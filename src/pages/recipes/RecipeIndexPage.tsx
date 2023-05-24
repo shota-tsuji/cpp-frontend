2bda3dcde4f91731c17691c0d376253e9f4309fc
shota-tsuji <shotatuzi@gmail.com>
2023-05-24 09:03:02 +0900

    Revert "feat: add Button to capture selected recipe details"
    This reverts commit b2ead133488a6a32f2ebcdcce0bb571ade5e40ef.

--- src/pages/recipes/RecipeIndexPage.tsx
+++ src/pages/recipes/RecipeIndexPage.tsx
@@ -1,32 +1,13 @@
-import React, {useMemo, useRef} from "react";
-import {Recipe, useRecipesQuery} from '../../generated/graphql';
-import {Button, Paper, Title} from '@mantine/core';
+import React from "react";
+import {useRecipesQuery} from '../../generated/graphql';
+import {Paper, Table, Title} from '@mantine/core';
 import RecipeAddButton from "../../features/recipes/RecipeAddButton";
 import {Link} from "react-router-dom";
-import type {MRT_ColumnDef} from 'mantine-react-table';
-import {MantineReactTable, MRT_TableInstance} from 'mantine-react-table';
 
 export default function RecipeIndexPage() {
     const [result, _reexecuteQuery] = useRecipesQuery();
     const {data, fetching, error} = result;
     console.log(JSON.stringify({fetching, data, error}, null, 2));
-    const tableInstanceRef = useRef<MRT_TableInstance<Recipe>>(null); //ts
-
-    const columns = useMemo<MRT_ColumnDef<Recipe>[]>(
-        () => [
-            {
-                accessorKey: 'title',
-                header: 'Title',
-            },
-            {
-                accessorKey: 'description',
-                header: 'Description',
-                mantineTableHeadCellProps: {sx: {color: 'green'}}, //custom props
-            },
-        ],
-        [],
-    );
-
 
     if (error != null) {
         return <>{JSON.stringify(error)}</>;
@@ -41,7 +22,12 @@ export default function RecipeIndexPage() {
         </tr>
     ));
 
-    /*
+    return (
+        <React.Fragment>
+            <Title order={1} mt="auto">Recipes</Title>
+            <Paper shadow="xs" mt="md" p="lg">
+
+            <RecipeAddButton/>
             <Table>
                 <thead>
                 <tr>
@@ -50,37 +36,6 @@ export default function RecipeIndexPage() {
                 </thead>
                 <tbody>{recipes}</tbody>
             </Table>
-     */
-
-    const someEventHandler = () => {
-        const rowSelection = tableInstanceRef.current.getState().rowSelection;
-        console.info(rowSelection);
-    };
-
-    return (
-        <React.Fragment>
-            <Title order={1} mt="auto">Recipes</Title>
-            <Paper shadow="xs" mt="md" p="lg">
-
-                <RecipeAddButton/>
-                <MantineReactTable
-                    columns={columns}
-                    data={data!.recipes}
-                    enableRowSelection //enable some features
-                    enableColumnOrdering
-                    enableGlobalFilter={false} //turn off a feature
-                    renderTopToolbarCustomActions={({table}) => {
-                        const handle = () => {
-                            console.info(table.getSelectedRowModel().flatRows);
-                        };
-
-                        return (
-                            <Button onClick={handle}>
-                                {'Do Something with Selected Rows'}
-                            </Button>
-                        );
-                    }}
-                />
             </Paper>
         </React.Fragment>
     );
